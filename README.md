5e9c03d65412d6f202cf9316b3147c910cc73cb9
Omar Abdulkhalek <polatak312@gmail.com>
2022-12-05 20:14:05 +0100

    Initial Commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# Programming Language
+
+* Kotlin
+
+# Frameworks
+
+* Spring boot 3.0.0
+
+# Dependencies
+
+* Spring Web
+* Spring Data JPA
+* MariaDB Driver
+
+# Testing Tool
+
+* Postman
---
+++ src/test/kotlin/com/abdulkhalekomar/library_api/LibraryApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.abdulkhalekomar.library_api
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class LibraryApiApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
