9fa1a30c4e03149deba1f4372a2d8f96176ce092
Omar Abdulkhalek <omar.abdulkhalek@amana.de>
2023-02-12 17:51:51 +0100

    make selected areas public

--- src/main/kotlin/com/abdulkhalekomar/library_api/security/ApplicationSecurityConfig.kt
+++ src/main/kotlin/com/abdulkhalekomar/library_api/security/ApplicationSecurityConfig.kt
@@ -2,6 +2,7 @@ package com.abdulkhalekomar.library_api.security
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpMethod
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.web.SecurityFilterChain
@@ -14,11 +15,14 @@ class ApplicationSecurityConfig {
 	fun filterChain(http: HttpSecurity): SecurityFilterChain {
 		http
 			.authorizeHttpRequests()
-			.requestMatchers("/", "index", "/css/*", "/js/*")
+			.requestMatchers(HttpMethod.GET, "/index.html")
 			.permitAll()
+			.anyRequest()
+			.authenticated()
 			.and()
 			.httpBasic()
 		return http.build()
 	}
+
 
 }
