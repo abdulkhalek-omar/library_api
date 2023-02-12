22feb63c233f2cf8efe07188079c479982ab2ea0
Omar Abdulkhalek <omar.abdulkhalek@amana.de>
2023-02-12 23:48:15 +0100

    In-Memory Authentication + PasswordEncoder

--- src/main/kotlin/com/abdulkhalekomar/library_api/security/ApplicationSecurityConfig.kt
+++ src/main/kotlin/com/abdulkhalekomar/library_api/security/ApplicationSecurityConfig.kt
@@ -1,16 +1,25 @@
 package com.abdulkhalekomar.library_api.security
 
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.http.HttpMethod
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.core.userdetails.User
+import org.springframework.security.core.userdetails.UserDetailsService
+import org.springframework.security.crypto.password.PasswordEncoder
+import org.springframework.security.provisioning.InMemoryUserDetailsManager
 import org.springframework.security.web.SecurityFilterChain
 
 
 @Configuration
 @EnableWebSecurity
-class ApplicationSecurityConfig {
+class ApplicationSecurityConfig(
+	@Autowired private val passwordEncoder: PasswordEncoder,
+) {
+
+
 	@Bean
 	fun filterChain(http: HttpSecurity): SecurityFilterChain {
 		http
@@ -24,5 +33,18 @@ class ApplicationSecurityConfig {
 		return http.build()
 	}
 
-
+	@Bean
+	fun users(): UserDetailsService {
+		val user = User.builder()
+			.username("user")
+			.password(passwordEncoder.encode("password"))
+			.roles("USER") // ROLE_USER
+			.build()
+		val admin = User.builder()
+			.username("admin")
+			.password(passwordEncoder.encode("password"))
+			.roles("USER", "ADMIN")
+			.build()
+		return InMemoryUserDetailsManager(user, admin)
+	}
 }
