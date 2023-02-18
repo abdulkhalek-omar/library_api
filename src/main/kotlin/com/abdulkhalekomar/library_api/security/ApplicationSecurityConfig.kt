2149e7cc92e2d07dcea4f798a1dea50fb7727b74
Omar Abdulkhalek <omar.abdulkhalek@amana.de>
2023-02-19 00:38:00 +0100

    Remember Me Cookie and Extra Options  (remember-me "2 weeks default" cookie)  contains:  username  expiration time  md5 hash of the above 2 values
     Note:
     tokenRepository() when using real DB

--- src/main/kotlin/com/abdulkhalekomar/library_api/security/ApplicationSecurityConfig.kt
+++ src/main/kotlin/com/abdulkhalekomar/library_api/security/ApplicationSecurityConfig.kt
@@ -13,6 +13,7 @@ import org.springframework.security.core.userdetails.UserDetailsService
 import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.security.provisioning.InMemoryUserDetailsManager
 import org.springframework.security.web.SecurityFilterChain
+import java.util.concurrent.TimeUnit
 
 
 @Configuration
@@ -34,7 +35,7 @@ class ApplicationSecurityConfig(
 			.loginPage("/login").permitAll()
 			.defaultSuccessUrl("/users", true)
 			.and()
-			.rememberMe() // defaults to 2 weeks
+			.rememberMe().tokenValiditySeconds(TimeUnit.DAYS.toSeconds(21).toInt()).key("somethingverysecured")
 		return http.build()
 	}
 
