3428d3cebdd76a0126661b523b6d46c0c4c10694
Omar Abdulkhalek <omar.abdulkhalek@amana.de>
2023-02-24 00:32:14 +0100

    springdoc-openapi added "https://springdoc.org/v2/"

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,6 +22,8 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-security")
 	implementation("org.springframework.boot:spring-boot-starter-test")
 
+	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.0.2")
+
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
