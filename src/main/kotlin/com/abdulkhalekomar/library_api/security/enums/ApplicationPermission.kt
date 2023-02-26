085cd834c908ee395c6fc235664fa27ddff751dc
Omar Abdulkhalek <omar.abdulkhalek@amana.de>
2023-02-26 14:39:46 +0100

    Clean Code -> will be newly created

--- src/main/kotlin/com/abdulkhalekomar/library_api/security/enums/ApplicationPermission.kt
+++
@@ -1,8 +0,0 @@
-package com.abdulkhalekomar.library_api.security.enums
-
-enum class ApplicationPermission(val permission: String) {
-	USER_READ("USER:READ"),
-	USER_WRITE("USER:WRITE"),
-	COURSE_READ("COURSE:READ"),
-	COURSE_WRITE("COURSE:WRITE"),
-}
